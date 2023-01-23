35b8269fc43bef90c33447fa46e4439ba5fce552
TrashyRicca <123311008+TrashyRicca@users.noreply.github.com>
2023-01-23 02:25:59 +0100

    bug fix

--- src/JavaScript/landingScreenAnim.js
+++ src/JavaScript/landingScreenAnim.js
@@ -1,52 +1,52 @@
-let landed = false;
-$(document).ready(function () {
-	if (!((navigator.userAgent.toLowerCase().indexOf('firefox') > -1) || (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)))) {
-		var dashOffset = parseInt($("#madSVG>g>path:first-child").css("stroke-dashoffset"), 10);
-		var scrollDistance = 1000;
-		var scrollDistance2 = 400;
-		var svgComplete = false;
-		var animationCompleted1 = false;
-		var animationCompleted2 = false;
-		$(window).scroll(function () {
-			if (!svgComplete) {
-				var percentageComplete = 1 / scrollDistance * $(window).scrollTop();
-				$("#madSVG>g>path:first-child").css("stroke-dashoffset", Math.max(dashOffset - (dashOffset * percentageComplete), 0));
-				$("#madSVG>g>path:not(:first-child)").css("stroke-dashoffset", Math.max(dashOffset - (dashOffset * percentageComplete / 6), 0));
-				if ($(window).scrollTop() > scrollDistance * 1.1) {
-					svgComplete = true;
-					window.scrollTo(0, 0);
-					$("#landingScreen").css("position", "relative");
-					$("#lSHeaderContainer").css("position", "fixed");
-					$("header").css("display", "block");
-				}
-			} else if (!animationCompleted1) {
-				var percentageComplete = 1 / scrollDistance2 * $(window).scrollTop();
-				// WARNING: 14vw
-				var fontSize = Math.max(((4 * $(window).height() / 100) + ((14 * $(window).width() / 100 - (4 * $(window).height() / 100)) * (1 - percentageComplete))), (4 * $(window).height() / 100));
-				$("#fadeOutContainer").css("transform", "translate(0,-" + Math.max((92 * (percentageComplete)) - (100 * ($(window).scrollTop() / $(window).height())), 0) + "vh");
-				$("#homepage").css("transform", "translate(0,-" + Math.max((92 * (percentageComplete)) - (100 * ($(window).scrollTop() / $(window).height())), 0) + "vh");
-				$("#scrollDown").css("opacity", Math.max(0.4 * (1 - percentageComplete * 2), 0));
-				$("#scrollDown").toggleClass("scrolldownAnim", false);
-				$("#lSHeader>span").css("font-size", (fontSize / $(window).height() * 100) + "vh");
-				$("#lSHeader>span").css("top", Math.max(50 - (50 * percentageComplete), 0) + "%");
-				$("#lSHeader>span").css("transform", "translate(-50%," + Math.min(-50 - (-50 * percentageComplete), 0) + "%");
-				$("#lSHeader").css("filter", "url(#threshold) blur(0px)");
-				if (percentageComplete >= 1) {
-					animationCompleted1 = true;
-					changeCooldownTime(2);
-					window.scrollTo(0, $(window).scrollTop() - scrollDistance2);
-					$("#fadeOutContainer").css("display", "none");
-					$("#homepage").css("transform", "translate(0,0)");
-					$("#landingScreen").css("height", "8vh");
-					$("#lSHeaderContainer").css("display", "none");
-					landed = true;
-				}
-			} else {
-				var percentageComplete = 1 / (document.documentElement.scrollHeight - $(window).height()) * $(window).scrollTop();
-				$("#navLine").css("width", Math.min(100 * percentageComplete, 100) + "%");
-			}
-		});
-	}
-	typeWriter();
-	landed = true;
-});
+// let landed = false;
+// $(document).ready(function () {
+// 	if (!((navigator.userAgent.toLowerCase().indexOf('firefox') > -1) || (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)))) {
+// 		var dashOffset = parseInt($("#madSVG>g>path:first-child").css("stroke-dashoffset"), 10);
+// 		var scrollDistance = 1000;
+// 		var scrollDistance2 = 400;
+// 		var svgComplete = false;
+// 		var animationCompleted1 = false;
+// 		var animationCompleted2 = false;
+// 		$(window).scroll(function () {
+// 			if (!svgComplete) {
+// 				var percentageComplete = 1 / scrollDistance * $(window).scrollTop();
+// 				$("#madSVG>g>path:first-child").css("stroke-dashoffset", Math.max(dashOffset - (dashOffset * percentageComplete), 0));
+// 				$("#madSVG>g>path:not(:first-child)").css("stroke-dashoffset", Math.max(dashOffset - (dashOffset * percentageComplete / 6), 0));
+// 				if ($(window).scrollTop() > scrollDistance * 1.1) {
+// 					svgComplete = true;
+// 					window.scrollTo(0, 0);
+// 					$("#landingScreen").css("position", "relative");
+// 					$("#lSHeaderContainer").css("position", "fixed");
+// 					$("header").css("display", "block");
+// 				}
+// 			} else if (!animationCompleted1) {
+// 				var percentageComplete = 1 / scrollDistance2 * $(window).scrollTop();
+// 				// WARNING: 14vw
+// 				var fontSize = Math.max(((4 * $(window).height() / 100) + ((14 * $(window).width() / 100 - (4 * $(window).height() / 100)) * (1 - percentageComplete))), (4 * $(window).height() / 100));
+// 				$("#fadeOutContainer").css("transform", "translate(0,-" + Math.max((92 * (percentageComplete)) - (100 * ($(window).scrollTop() / $(window).height())), 0) + "vh");
+// 				$("#homepage").css("transform", "translate(0,-" + Math.max((92 * (percentageComplete)) - (100 * ($(window).scrollTop() / $(window).height())), 0) + "vh");
+// 				$("#scrollDown").css("opacity", Math.max(0.4 * (1 - percentageComplete * 2), 0));
+// 				$("#scrollDown").toggleClass("scrolldownAnim", false);
+// 				$("#lSHeader>span").css("font-size", (fontSize / $(window).height() * 100) + "vh");
+// 				$("#lSHeader>span").css("top", Math.max(50 - (50 * percentageComplete), 0) + "%");
+// 				$("#lSHeader>span").css("transform", "translate(-50%," + Math.min(-50 - (-50 * percentageComplete), 0) + "%");
+// 				$("#lSHeader").css("filter", "url(#threshold) blur(0px)");
+// 				if (percentageComplete >= 1) {
+// 					animationCompleted1 = true;
+// 					changeCooldownTime(2);
+// 					window.scrollTo(0, $(window).scrollTop() - scrollDistance2);
+// 					$("#fadeOutContainer").css("display", "none");
+// 					$("#homepage").css("transform", "translate(0,0)");
+// 					$("#landingScreen").css("height", "8vh");
+// 					$("#lSHeaderContainer").css("display", "none");
+// 					landed = true;
+// 				}
+// 			} else {
+// 				var percentageComplete = 1 / (document.documentElement.scrollHeight - $(window).height()) * $(window).scrollTop();
+// 				$("#navLine").css("width", Math.min(100 * percentageComplete, 100) + "%");
+// 			}
+// 		});
+// 	}
+// 	typeWriter();
+// 	landed = true;
+// });
--- src/JavaScript/landingScreenTextAnim.js
+++ src/JavaScript/landingScreenTextAnim.js
@@ -1,89 +1,89 @@
-let cooldownTime = 0.5;
+// let cooldownTime = 0.5;
 
-$(document).ready(function() {
-	const elts = {
-		text1: document.getElementById("text1"),
-		text2: document.getElementById("text2")
-	};
+// $(document).ready(function() {
+// 	const elts = {
+// 		text1: document.getElementById("text1"),
+// 		text2: document.getElementById("text2")
+// 	};
 
-	const texts = [
-		"44 LABS",
-		"PROVIDING",
-		"SOLUTIONS",
-		"IN WEB3"
-	];
+// 	const texts = [
+// 		"44 LABS",
+// 		"PROVIDING",
+// 		"SOLUTIONS",
+// 		"IN WEB3"
+// 	];
 
-	const morphTime = 1;
+// 	const morphTime = 1;
 
-	let textIndex = texts.length - 1;
-	let time = new Date();
-	let morph = 0;
-	let cooldown = cooldownTime;
+// 	let textIndex = texts.length - 1;
+// 	let time = new Date();
+// 	let morph = 0;
+// 	let cooldown = cooldownTime;
 
-	elts.text1.textContent = texts[textIndex % texts.length];
-	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+// 	elts.text1.textContent = texts[textIndex % texts.length];
+// 	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
-	function doMorph() {
-		morph -= cooldown;
-		cooldown = 0;
+// 	function doMorph() {
+// 		morph -= cooldown;
+// 		cooldown = 0;
 
-		let fraction = morph / morphTime;
+// 		let fraction = morph / morphTime;
 
-		if (fraction > 1) {
-			cooldown = cooldownTime;
-			fraction = 1;
-		}
+// 		if (fraction > 1) {
+// 			cooldown = cooldownTime;
+// 			fraction = 1;
+// 		}
 
-		setMorph(fraction);
-	}
+// 		setMorph(fraction);
+// 	}
 
-	function setMorph(fraction) {
-		elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-		elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+// 	function setMorph(fraction) {
+// 		elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+// 		elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-		fraction = 1 - fraction;
-		elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-		elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+// 		fraction = 1 - fraction;
+// 		elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+// 		elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-		elts.text1.textContent = texts[textIndex % texts.length];
-		elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-	}
+// 		elts.text1.textContent = texts[textIndex % texts.length];
+// 		elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+// 	}
 
-	function doCooldown() {
-		morph = 0;
+// 	function doCooldown() {
+// 		morph = 0;
 
-		elts.text2.style.filter = "";
-		elts.text2.style.opacity = "100%";
+// 		elts.text2.style.filter = "";
+// 		elts.text2.style.opacity = "100%";
 
-		elts.text1.style.filter = "";
-		elts.text1.style.opacity = "0%";
-	}
+// 		elts.text1.style.filter = "";
+// 		elts.text1.style.opacity = "0%";
+// 	}
 
-	function animate() {
-		requestAnimationFrame(animate);
+// 	function animate() {
+// 		requestAnimationFrame(animate);
 
-		let newTime = new Date();
-		let shouldIncrementIndex = cooldown > 0;
-		let dt = (newTime - time) / 1000;
-		time = newTime;
-		cooldown -= dt;
+// 		let newTime = new Date();
+// 		let shouldIncrementIndex = cooldown > 0;
+// 		let dt = (newTime - time) / 1000;
+// 		time = newTime;
+// 		cooldown -= dt;
 
-		if (cooldown <= 0) {
-			if (shouldIncrementIndex) {
-				textIndex++;
-			}
+// 		if (cooldown <= 0) {
+// 			if (shouldIncrementIndex) {
+// 				textIndex++;
+// 			}
 
-			doMorph();
-		} else {
-			doCooldown();
-		}
-	}
+// 			doMorph();
+// 		} else {
+// 			doCooldown();
+// 		}
+// 	}
 	
-	if (!((navigator.userAgent.toLowerCase().indexOf('firefox') > -1) || (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)))) {
-        animate();
-    }
-});
+// 	if (!((navigator.userAgent.toLowerCase().indexOf('firefox') > -1) || (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)))) {
+//         animate();
+//     }
+// });
 
-function changeCooldownTime(time) {
-	cooldownTime = time;
-}
+// function changeCooldownTime(time) {
+// 	cooldownTime = time;
+// }
